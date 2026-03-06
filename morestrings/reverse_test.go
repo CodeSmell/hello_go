905100231a1de9aff955e0625c7cc96d67be4c82
codemsell <mbarlotta@gmail.com>
2026-03-06 12:17:33 -0500

    add testify/assert

--- morestrings/reverse_test.go
+++ morestrings/reverse_test.go
@@ -2,6 +2,8 @@ package morestrings
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestReverseRunes_empty(t *testing.T) {
@@ -20,6 +22,19 @@ func TestReverseRunes(t *testing.T) {
 	}
 }
 
+func TestReverseRunes_empty_asserts(t *testing.T) {
+	in := ""
+	result := ReverseRunes(in)
+	assert.Equal(t, "", result, "empty in, empty out")
+}
+
+func TestReverseRunes_asserts(t *testing.T) {
+	in := "The answer to the Ultimate Question of Life, The Universe, and Everything"
+	want := "gnihtyrevE dna ,esrevinU ehT ,efiL fo noitseuQ etamitlU eht ot rewsna ehT"
+	result := ReverseRunes(in)
+	assert.Equal(t, want, result, "non-empty in, reversed out")
+}
+
 func TestReverseRunesWithErr_empty(t *testing.T) {
 	in := ""
 	want := ""
@@ -28,10 +43,25 @@ func TestReverseRunesWithErr_empty(t *testing.T) {
 	}
 }
 
+func TestReverseRunesWithErr_empty_asserts(t *testing.T) {
+	in := ""
+	result, err := ReverseRunesWithErr(in)
+	assert.NotNil(t, err, "empty in, error expected")
+	assert.Equal(t, "", result, "empty in, empty out")
+}
+
 func TestReverseRunesWithErr(t *testing.T) {
 	in := "The answer to the Ultimate Question of Life, The Universe, and Everything"
 	want := "gnihtyrevE dna ,esrevinU ehT ,efiL fo noitseuQ etamitlU eht ot rewsna ehT"
 	if got, err := ReverseRunesWithErr(in); err != nil || got != want {
 		t.Errorf("ReverseRunesWithErr(%q) = %q, want %q, err = %v", in, got, want, err)
 	}
+}
+
+func TestReverseRunesWithErr_asserts(t *testing.T) {
+	in := "The answer to the Ultimate Question of Life, The Universe, and Everything"
+	want := "gnihtyrevE dna ,esrevinU ehT ,efiL fo noitseuQ etamitlU eht ot rewsna ehT"
+	result, err := ReverseRunesWithErr(in)
+	assert.NoError(t, err, "non-empty in, no error expected")
+	assert.Equal(t, want, result, "non-empty in, reversed out")
 }
